e0108c1eb97f241b71b2cb6f7f87d7ac44819a7a
Lautaro Gomez <lautaro.m.gomez42@gmail.com>
2022-07-20 17:12:56 -0300

    commit 1

---
+++ Mi_App/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+def saludo(request):
+    return HttpResponse("hola mundo")
+
+def saludar_a(request, nombre):
+    return HttpResponse(f"hola com estas{nombre}")
+    
